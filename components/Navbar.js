ca522227f7a357e41a7f63bd359ddf76279370d8
Krishna <nayak100krishna@gmail.com>
2022-01-01 15:28:02 +0530

    Remove Head tag added some tailwindcss class

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,23 +4,25 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header className={styles.header}>
-      <Link href="/">
-        <a>
-          <Image src="/vercel.svg" width={120} height={80} />
-        </a>
-      </Link>
-      <nav className={styles.nav}>
-        <Link href="/contact">
-          <a>Contact us</a>
+    <header className="container fixed w-full z-10 left-1/2 -translate-x-1/2 px-4">
+      <div className="flex justify-between items-center">
+        <Link href="/">
+          <a>
+            <Image src="/vercel.svg" width={120} height={80} />
+          </a>
         </Link>
-        <Link href="/register">
-          <a>registor</a>
-        </Link>
-        <Link href="/about">
-          <a>About us</a>
-        </Link>
-      </nav>
+        <nav>
+          <Link href="/contact">
+            <a>Contact us</a>
+          </Link>
+          <Link href="/register">
+            <a className="px-3">Registor</a>
+          </Link>
+          <Link href="/about">
+            <a>About us</a>
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
