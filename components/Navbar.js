6f48a840f5303b77ffdca42f0aee338b7e7e1108
Krishna Nayak <72739429+krishna-nayak@users.noreply.github.com>
2022-03-03 16:05:48 +0530

    Layout change

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,19 @@
 // import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
+import { useEffect } from "react";
 
 const Navbar = () => {
+  const onScreenChange = (e) => {
+    // if(e.)
+    log();
+  };
   return (
     <header className="w-full text-center mx-auto">
-      <nav className="p-4 space-x-5 ">
+      <nav className="p-4 space-x-5 " style={{ display: "none" }}>
         <Link href="/">
           <a>Home</a>
         </Link>
-        <Link href="register">
+        <Link href="/register">
           <a>Registration</a>
         </Link>
         <Link href="#speaker">
