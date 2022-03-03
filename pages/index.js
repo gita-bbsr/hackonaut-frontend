80a876c7b0c1360c8a0a47078d478a351094c9b7
Krishna Nayak <72739429+krishna-nayak@users.noreply.github.com>
2022-03-03 21:24:05 +0530

    responsive added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import rocketPic from "../public/rocket.png";
-import hero from "../public/svg-file/hero.svg";
-import friends from "../public/svg-file/friends.svg";
-import rocketEntry from "../public/svg-file/rocket-entrie.svg";
-import learn from "../public/svg-file/learn.svg";
+import hero from "../public/svg/hero.svg";
+import friends from "../public/svg/friends.svg";
+import rocketEntry from "../public/svg/rocket-entrie.svg";
+import learn from "../public/svg/learn.svg";
+import grow from "../public/svg/grow.svg";
+import build from "../public/svg/build.svg";
+
+import useSize from "../lib/hooks.js";
 
 const check = [
     {
@@ -24,10 +28,25 @@ const check = [
         main: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, assumenda dolorem voluptatibus in iusto suscipit deserunt delectus consectetur beatae, iure perferendis, maiores enim. In, eum corrupti? Magnam architecto tempora illum.",
         img: learn,
     },
+    {
+        id: 4,
+        head: "Build",
+        main: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, assumenda dolorem voluptatibus in iusto suscipit deserunt delectus consectetur beatae, iure perferendis, maiores enim. In, eum corrupti? Magnam architecto tempora illum.",
+        img: build,
+    },
+    {
+        id: 5,
+        head: "Grow",
+        main: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, assumenda dolorem voluptatibus in iusto suscipit deserunt delectus consectetur beatae, iure perferendis, maiores enim. In, eum corrupti? Magnam architecto tempora illum.",
+        img: grow,
+    },
 ];
 
 export default function Home() {
-    const featureList = check.map((obj) => <Feature key={obj.id.toString()} valueObj={obj} />);
+    const size = useSize();
+    console.log(size);
+    const featureMoblieList = check.map((obj) => <FeatureMoblie key={obj.id.toString()} valueObj={obj} />);
+    const featureWebList = check.map((obj) => <FeatureWeb key={obj.id.toString()} valueObj={obj} />);
     return (
         <>
             <div className="h-32"></div>
@@ -44,27 +63,56 @@ export default function Home() {
                 </div>
 
                 <div>
-                    <h2 className="lg:text-6xl text-3xl text-justify font-extrabold">
+                    <h2 className="lg:text-6xl text-3xl text-justify font-extrabold my-10">
                         The year 2022 is unlike any other; therefore, this edition of Hack‘O’naut should be unlike any others. Get out of your tight
                         denims and hoodies, hop into SOMETHING COMFY; let’s #BUILD-SOMETHING-NEW
                     </h2>
                 </div>
                 {/* feature */}
-                <div className="text-center ">{featureList}</div>
+                <div className="lg:block flex flex-col space-y-8">{size.width < 1024 ? featureMoblieList : featureWebList}</div>
             </main>
         </>
     );
 }
 
-function Feature(props) {
+function FeatureMoblie(props) {
     const value = props.valueObj;
     return (
-        <div>
-            <div className="md:w-auto w-80 mx-auto">
+        <div className="text-center">
+            <div className="w-72 md:w-96 mx-auto">
                 <Image src={value.img} />
             </div>
-            <h2 className="lg:text-6xl text-3xl font-extrabold">{value.head}</h2>
-            <p className="font-extrabold">{value.main}</p>
+            <div>
+                <h2 className="lg:text-6xl text-3xl font-extrabold">{value.head}</h2>
+                <p className="font-extrabold mx-auto mt-4 text-justify sm:px-20">{value.main}</p>
+            </div>
         </div>
     );
+}
+
+function FeatureWeb(props) {
+    const value = props.valueObj;
+    const leftComponent = (
+        <div className="text-left grid grid-cols-2 gap-4">
+            <div>
+                <Image src={value.img} />
+            </div>
+            <div>
+                <h2 className="lg:text-6xl text-3xl font-extrabold">{value.head}</h2>
+                <p className=" text-3xl font-extrabold mx-auto mt-4 text-justify ">{value.main}</p>
+            </div>
+        </div>
+    );
+    const rightComponent = (
+        <div className="text-left grid grid-cols-2 gap-4">
+            <div>
+                <h2 className="lg:text-6xl text-3xl font-extrabold">{value.head}</h2>
+                <p className=" text-3xl font-extrabold mx-auto mt-4 text-justify ">{value.main}</p>
+            </div>
+            <div>
+                <Image src={value.img} />
+            </div>
+        </div>
+    );
+    return value.id % 2 == 0 ? rightComponent : leftComponent;
 }
