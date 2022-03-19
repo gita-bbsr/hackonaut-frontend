f7b3230c5bada301a4d6c68fdf75c46254e31d64
Krishna <nayak100krishna@gmail.com>
2022-03-19 14:58:18 +0530

    sponsors grid layout

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,20 +1,30 @@
 import Image from "next/image";
 import write from "../public/msg.png";
 
+import classroom from "../public/image/class.jpeg";
+import lead from "../public/image/lead.jpeg";
+import team from "../public/image/team.jpeg";
+
 const gallery = () => {
   return (
     <div className="relative grid lg:grid-cols-3 my-40 z-20 gap-10">
       <div>
-        <div className="w-full h-80 bg-black mx-auto"></div>
+        <div className="w-full  mx-auto">
+          <Image src={lead} />
+        </div>
         <p>Lorem ipsum dolor sit amet consectetur.</p>
       </div>
       <div className="md:relative md:top-24 ">
-        <div className="w-full h-64 bg-black mx-auto"></div>
+        <div className="w-full mx-auto">
+          <Image src={classroom} />
+        </div>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
       </div>
       <div>
-        <div className="w-full h-80 bg-black mx-auto"></div>
+        <div className="w-full mx-auto">
+          <Image src={team} />
+        </div>
 
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
--- components/Sponsors.js
+++ components/Sponsors.js
@@ -1,10 +1,18 @@
+import Image from "next/image";
 const sponsors = () => {
   return (
     <section id="sponsors" className="p-28 text-center space-y-4">
       <h2>Our Sponsour</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere?</p>
       {/* sponsour list grid */}
-      <div className="p-32"></div>
+      <div className="py-20 grid grid-cols-[repeat(4,minmax(150px,_1fr))] grid-flow-row gap-4">
+        <Image src="https://www.w3schools.com/howto/img_avatar.png" width={100} height={300} />
+        <Image src="https://www.w3schools.com/howto/img_avatar.png" width={100} height={100} />
+        <Image src="https://www.w3schools.com/howto/img_avatar.png" width={100} height={100} />
+        <Image src="https://www.w3schools.com/howto/img_avatar.png" width={100} height={100} />
+        {/* hegiht will vary there size */}
+        <Image src="https://www.w3schools.com/howto/img_avatar.png" width={100} height={300} />
+      </div>
     </section>
   );
 };
