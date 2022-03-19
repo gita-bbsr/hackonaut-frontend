8578a106f1d5e69057f42bea79990da144757c7e
Krishna <nayak100krishna@gmail.com>
2022-03-19 14:58:23 +0530

    price container

--- components/Prices.js
+++ components/Prices.js
@@ -2,9 +2,26 @@ const price = () => {
   return (
     <section id="prices" className="text-center">
       <h2>Prices</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+      <p>The Top will be reawarded with awesome prices</p>
       {/* price deck section */}
-      <div className="p-32"></div>
+      <div className="p-20 grid grid-cols-3 grid-flow-row">
+        <div className="border py-5">
+          <h4>No. 1 Leading Team</h4>
+          <h3>$5000</h3>
+          <br />
+          <p>Perks</p>
+          <ul>
+            <li>bals</li>
+            <li>bals</li>
+            <li>bals</li>
+            <li>bals</li>
+            <li>bals</li>
+            <li>bals</li>
+            <li>bals</li>
+            <li>bals</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
