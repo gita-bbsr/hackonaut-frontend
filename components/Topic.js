4abf0f4866505517c4adccfd324013000ee72f2d
Krishna <nayak100krishna@gmail.com>
2022-03-19 14:02:02 +0530

    Topic grid data insert

--- components/Topic.js
+++ components/Topic.js
@@ -1,10 +1,34 @@
 const topic = () => {
   return (
-    <section id="topic" className="text-center p-28">
+    <section id="topic" className="text-center p-28 bg-slate-100">
       <h2>Topics You Can Explore</h2>
       <p>we are Providing some of the area/domain in which you can bring your ideas.</p>
-      {/* topic grid */}
-      <div className="p-40"></div>
+      <div className="p-40 grid grid-cols-12 row-auto gap-3">
+        <div className="col-span-4 p-5 bg-white rounded-md">
+          <h4>Fintech/Neobanking/kyc</h4>
+        </div>
+        <div className="col-span-4">
+          <h4>Fintech/Neobanking/kyc</h4>
+        </div>
+        <div className="col-span-4">
+          <h4>Fintech/Neobanking/kyc</h4>
+        </div>
+        <div className="col-start-3 col-span-4">
+          <h4>Fintech/Neobanking/kyc</h4>
+        </div>
+        <div className="col-span-4">
+          <h4>Fintech/Neobanking/kyc</h4>
+        </div>
+        <div className="col-span-4">
+          <h4>Fintech/Neobanking/kyc</h4>
+        </div>
+        <div className="col-span-4">
+          <h4>Fintech/Neobanking/kyc</h4>
+        </div>
+        <div className="col-span-4">
+          <h4>Fintech/Neobanking/kyc</h4>
+        </div>
+      </div>
     </section>
   );
 };
